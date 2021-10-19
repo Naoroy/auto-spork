9754d134aff8c321740716d2402d40fdcb9c937b
Lint Action <lint-action@samuelmeuli.com>
2021-10-19 15:32:52 +0000

    Fix code style issues with ESLint

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const app = express()
 
-
 app.route('/')
-  .get(function(req, res) {
-    res.send({ msg: "Hello Spork" })
+  .get(function (req, res) {
+    res.send({ msg: 'Hello Spork' })
   })
 app.route('/articles')
-  .get(function(req, res) {
+  .get(function (req, res) {
     res.send([
-      { 
+      {
         title: '',
         content: '',
         date: new Date(),
         author: '',
-        source: '',
+        source: ''
       }
     ])
   })
-
 
 if (!module.parent) app.listen(PORT)
 module.exports = app
--- test/server.test.js
+++ test/server.test.js
@@ -3,11 +3,10 @@ const chaiHttp = require('chai-http')
 const server = require('../server')
 const should = chai.should()
 
-
 chai.use(chaiHttp)
 
-describe('GET /', function() {
-  it('Returns a valid JSON response', function() {
+describe('GET /', function () {
+  it('Returns a valid JSON response', function () {
     chai.request(server)
       .get('/')
       .end(function (err, res) {
@@ -21,68 +20,67 @@ describe('GET /', function() {
       })
   })
   it('Returns a response with { msg: "arbitrary string" }',
-  function() {
-    chai.request(server)
-      .get('/')
-      .end(function (err, res) {
-        res.body.should.be.a('object')
-        res.body.should.have.property("msg")
-        res.body.msg.should.be.a('string')
-      })
-  })
+    function () {
+      chai.request(server)
+        .get('/')
+        .end(function (err, res) {
+          res.body.should.be.a('object')
+          res.body.should.have.property('msg')
+          res.body.msg.should.be.a('string')
+        })
+    })
 })
-describe('GET /articles', function() {
-  describe('GET /articles', function() {
-  it('Returns an array', function() {
-    chai.request(server)
-      .get('/articles')
-      .end(function (err, res) {
-        const articles = res.body
+describe('GET /articles', function () {
+  describe('GET /articles', function () {
+    it('Returns an array', function () {
+      chai.request(server)
+        .get('/articles')
+        .end(function (err, res) {
+          const articles = res.body
 
-        articles.should.be.a('array')
-      })
-  })
-  it('Returns an array with a dummmy articles object', function() {
-    chai.request(server)
-      .get('/articles')
-      .end(function (err, res) {
-        const articles = res.body
-        const firstArticle = articles[0]
+          articles.should.be.a('array')
+        })
+    })
+    it('Returns an array with a dummmy articles object', function () {
+      chai.request(server)
+        .get('/articles')
+        .end(function (err, res) {
+          const articles = res.body
+          const firstArticle = articles[0]
 
-        articles.should.be.not.empty
-        firstArticle.should.be.a('object')
-      })
+          articles.should.be.not.empty
+          firstArticle.should.be.a('object')
+        })
+    })
   })
-  })
-  describe.only('Valid article properties', function() {
+  describe.only('Valid article properties', function () {
     let article
-    before(function(done) {
+    before(function (done) {
       chai.request(server)
         .get('/articles')
         .end(function (err, res) {
           article = res.body[0]
           done()
-      })
+        })
     })
-    it('Has a "title" property', function() {
+    it('Has a "title" property', function () {
       article.should.have.property('title')
       article.title.should.be.a('string')
     })
-    it('Has a "content" property', function() {
+    it('Has a "content" property', function () {
       article.should.have.property('content')
       article.content.should.be.a('string')
     })
-    xit('Has a "date" property', function() {
+    xit('Has a "date" property', function () {
       article.should.have.property('date')
     })
-    it('Has an "author" property', function() {
+    it('Has an "author" property', function () {
       article.should.have.property('author')
       article.author.should.be.a('string')
     })
-    it('Has a "source" property', function() {
+    it('Has a "source" property', function () {
       article.should.have.property('source')
       article.source.should.be.a('string')
     })
   })
 })
-
